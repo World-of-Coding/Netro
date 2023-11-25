6dc106ea9ec7c92c6807382a9eb44717dbe44d17
The Bunny <thebunny@skiff.com>
2023-11-25 16:31:09 -0700

    Update events.js
    Found a better event handler

--- handler/events.js
+++ handler/events.js
@@ -1,37 +1,23 @@
+const ascii = require("ascii-table");
+const fs = require("fs");
+
 function loadEvents(client) {
-    const ascii = require("ascii-table");
-    const fs = require("fs");
-    const table = new ascii().setHeading("Events", "Working");
-  
-    const folder = fs.readdirSync("./Events")
-      const files = fs
-        .readdirSync(`./Events/${folder}`)
-        .filter((file) => file.endsWith(".js"));
-  
-      for (const file of files) {
-        const event = require(`../Events/${file}`);
-  
-        if (event.rest) {
-          if (event.once)
-            client.rest.once(event.name, (...args) =>
-              event.execute(...args, client)
-            );
-            else 
-              client.rest.on(event.name, (...args) =>
-              event.execute(...args, client)
-              ); 
-              
-        } else {
-          if(event.once) 
-          client.once(event.name, (...args) =>
-          event.execute(...args, client));
-          else client.on(event.name, (...args) =>
-          event.execute(...args, client));
+    const eventTable = new ascii().setHeading("Events", "Working");
+
+    for(const eventFolder of eventFolderRoot){
+
+        const eventFiles = fs.readdirSync(`./events/${eventFolder}`).filter((file) => file.endsWith(".js"));
+
+        for(const event of eventFiles){
+            const masterEvent = require(`../events/${eventFolder}/${event}`);
+
+            client.on(masterEvent.name, (...args) => masterEvent.execute(...args, client) );
+
+            eventTable.addRow(event, "Yes");
         }
-        table.addRow(file, "yes")
-        continue;
-      }
-    return console.log(table.toString(), "\nLoaded Events   ")
+    }
+
+    console.log(eventTable.toString());
 }
   
 module.exports = { loadEvents }
