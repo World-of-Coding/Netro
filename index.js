55d37dd0bd8381293578f8ea9b7db8527e85b776
The Bunny <thebunny@skiff.com>
2023-11-25 16:32:06 -0700

    Update index.js
    Uses a better index.js version.

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
-const { Client, Collection, Intents } = require('discord.js');
+const { Client, Collection, GatewayIntentBits, Partials } = require('discord.js');
+const { QuickDB } = require("quick.db");
 
 const ModmailManager = require('./utils/modmail');
 
 const client = new Client({
   intents: [
-    Intents.FLAGS.GUILDS,
-    Intents.FLAGS.GUILD_MESSAGES,
-    Intents.FLAGS.GUILD_MEMBERS,
-    Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
-    Intents.FLAGS.GUILD_PRESENCES,
-    Intents.FLAGS.GUILD_BANS,
-    Intents.FLAGS.DIRECT_MESSAGES,
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessageReactions,
+    GatewayIntentBits.GuildPresences,
+    GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildVoiceStates,
+    GatewayIntentBits.GuildEmojisAndStickers
   ],
-  partials: ['CHANNEL', 'REACTION', 'GUILD_SCHEDULED_EVENT', 'GUILD_MEMBER', 'USER', 'MESSAGE'],
+  partials: [
+    Partials.Channel, 
+    Partials.DirectMessages, 
+    Partials.Message, 
+    Partials.GuildMessageReactions, 
+    Partials.Reaction, 
+    Partials.GuildScheduledEvent, 
+    Partials.GuildMember, 
+    Partials.User
+  ],
 });
 module.exports = client;
 
@@ -23,13 +35,25 @@ client.cooldowns = new Collection();
 client.modmails = new Collection();
 client.modmailMan = new ModmailManager(client, true);
 client.config = require('./config.json');
-client.db = require('quick.db');
+client.db = new QuickDB();
+client.thanks = client.db.table("points");
+client.topicCooldown = new Collection();
+client.topicCooldownSec = 900;
 
 // Initializing the project
-require('./handler')(client);
-require('./dummyWeb')();
+const { loadCommands } = require("./handler/index");
+const { loadEvents } = require("./handler/events");
 
-client.login(client.config.token);
+client.setMaxListeners(15);
+
+client
+.login(client.config.token)
+.then(() => {
+  loadCommands(client);
+  loadEvents(client);
+})
+.catch((error) => console.log(error));
+
 
 process.on("uncaughtException", (err, origin) => {
     console.error(`Caught exception ${err} at ${origin}`);
